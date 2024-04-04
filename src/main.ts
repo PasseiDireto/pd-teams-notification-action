e725755b4ec264ede912aa42844c2d615993304a
Mateus Machado <matvieira@uolinc.com>
2024-04-04 19:57:18 -0300

    ci: space

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import axios from 'axios';
 import moment from 'moment-timezone';
 import { createMessageCard } from './message-card';
 
-
 async function run(): Promise<void> {
   try {
     const githubToken = core.getInput('github-token', { required: true });
